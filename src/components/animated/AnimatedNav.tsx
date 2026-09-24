1f8e2af1e3e30115af938a5c211f9d5b8a9dcb4d
Aditya <90863792+Aditya75117@users.noreply.github.com>
2026-09-24 16:23:48 +0530

    Add missing import statement in AnimatedNav component

--- src/components/animated/AnimatedNav.tsx
+++ src/components/animated/AnimatedNav.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useCallback, useEffect, useRef, useState } from "react";
 import { animatedView } from "@/data/animatedView";
 import { Icon } from "@/components/icons/Icon";
